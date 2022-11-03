072443254e9fc0b6071cc89b611f3ee3d9e2dd83
zikrizm <zikrimarifatullah024@gmail.com>
2022-11-03 10:25:58 +0700

    test

--- app/Http/Controllers/AttendanceCardController.php
+++ app/Http/Controllers/AttendanceCardController.php
@@ -188,7 +188,7 @@ class AttendanceCardController extends Controller
                 }
 
                 // * Employee search
-                $search = 'Erwan';
+                $search = '';
                 if (!empty($request->input('q'))) {
                     $search = $request->q;
                 }
@@ -206,10 +206,10 @@ class AttendanceCardController extends Controller
                 $attenDBs = [];
                 $slug_week = ['mgg', 'sen', 'sel', 'rab', 'kam', 'jum', 'sab'];
                 if (!empty($request->input('date'))) {
-                    $start_time = Carbon::parse("2022-10-16 23:59:59");
-                    $end_time = Carbon::parse("2022-10-25 23:59:59");
-                    // $start_time = Carbon::parse($request->date['start_time']);
-                    // $end_time = Carbon::parse($request->date['end_time']);
+                    // $start_time = Carbon::parse("2022-10-16 23:59:59");
+                    // $end_time = Carbon::parse("2022-10-25 23:59:59");
+                    $start_time = Carbon::parse($request->date['start_time']);
+                    $end_time = Carbon::parse($request->date['end_time']);
                     $filter['start_time'] = $start_time->hour(0)->minute(0)->second(0)->format('Y-m-d H:i:s');
                     $filter['end_time'] = $end_time->addHours(1)->hour(23)->minute(59)->second(59)->format('Y-m-d H:i:s');
                     $attenDBs = Transaction::whereBetween('punch_time', [$filter['start_time'], $filter['end_time']])->get();
